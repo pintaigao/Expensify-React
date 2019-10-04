97e32b43227e371f5b7a5dbbe787f61c75f5d268
hptg1994 <hptg1994@gmail.com>
2019-10-03 20:41:35 -0500

    so much for today

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import numeral from 'numeral';
-import selectExpenses from '../selectors/expenses';
-import selectExpensesTotal from '../selectors/expenses-total';
 import IosAdd from "react-ionicons/lib/IosAdd";
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
-
+export const ExpensesSummary = () => {
   return (
     <div className="page-header">
       <div className="content-container">
-        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+
         <Link className="button" to="/create">
           <div className="add-expense-button">
             <IosAdd color="white" fontSize="40px" />
@@ -24,13 +17,4 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  const visibleExpenses = selectExpenses(state.expenses, state.filters);
-
-  return {
-    expenseCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
-  };
-};
-
-export default connect(mapStateToProps)(ExpensesSummary);
+export default ExpensesSummary;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+import numeral from 'numeral';
 
-export const Header = ({ startLogout }) => (
-  <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        <Link className="header__title" to="/dashboard">
-          <h1>What I Spendx</h1>
-        </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+export const Header = ({ startLogout, expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <header className="header">
+      <div className="content-container">
+        <div className="header-title">
+          <Link to="/dashboard" style={{ textDecoration: 'none', color: "white" }}>
+            <h1>What I Spendx</h1>
+          </Link>
+
+          <div className="header-summary">
+            <h1 className="header-summary-title">Viewing</h1>
+            <h1> <span>{expenseCount}</span> {expenseWord}  </h1>
+            <h1> {formattedExpensesTotal}</h1>
+          </div>
+          <button className="logout-button" onClick={startLogout}>Logout</button>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   startLogout: () => dispatch(startLogout())
 });
 
-export default connect(undefined, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
