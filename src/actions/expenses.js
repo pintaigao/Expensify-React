daa4688197b5e5430dc841a0bf99c8dce8c524dd
pintaigao <pintaigao@gmail.com>
2019-11-10 11:27:42 -0600

    I dont know what being update

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -12,9 +12,9 @@ export const startAddExpense = (expenseData = {}) => {
     const uid = getState().auth.uid;
     const {
       description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
+      note = '',
+      amount = 0,
+      createdAt = 0
     } = expenseData;
     const expense = {
       description,
@@ -33,22 +33,16 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
 // REMOVE_EXPENSE
-export const removeExpense = ({
-  id
-} = {}) => ({
+export const removeExpense = ({id} = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
-export const startRemoveExpense = ({
-  id
-} = {}) => {
+export const startRemoveExpense = ({id} = {}) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
     return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
-      dispatch(removeExpense({
-        id
-      }));
+      dispatch(removeExpense({id}));
     });
   };
 };
